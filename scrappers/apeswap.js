6cc884b8f278c27b704ac2c156f59442d325da86
hannawaffle <hanna.waffle@gmail.com>
2021-03-29 17:56:40 -0300

    add apeswap

---
+++ scrappers/apeswap.js
@@ -0,0 +1,34 @@
+const id = 'apeswap';
+
+const url = 'https://apeswap.finance/pools';
+
+const delay = 10000;
+
+var onScrap = () => {
+  const poolCards = document.querySelectorAll('.sc-bTvRPi');
+
+  const info = [...poolCards].map((poolCard) => {
+    const title = poolCard.querySelector('.sc-kIeTtH').textContent.trim()
+    if (title.indexOf('Your Project') === 0) {
+        return;
+    }
+    const earnToken = title.split(' ')[0];
+    const stakeToken = earnToken;
+    const APR = poolCard.querySelector('.sc-jmhFOf .sc-gsTCUz').textContent;
+
+    return ({
+      stakeToken,
+      APR,
+      earnToken
+    })
+  });
+
+  return info.filter(i => i);
+}
+
+module.exports = {
+  delay,
+  id,
+  onScrap,
+  url
+};
--- scrappers/index.js
+++ scrappers/index.js
@@ -1,9 +1,11 @@
+const apeswap = require('./apeswap');
 const goosedefi = require('./goosedefi');
 const pancakeswap = require('./pancakeswap');
 const pandayield = require('./pandayield');
 const saltswap = require('./saltswap');
 
 const scrappers = [
+  apeswap,
   goosedefi,
   pancakeswap,
   pandayield,
