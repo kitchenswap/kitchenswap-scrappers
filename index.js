9e58d6b51ce534385ac6ffa68331fcf92a5e8697
hannawaffle <hanna.waffle@gmail.com>
2021-03-28 21:01:34 -0300

    try playwright instead of puppeteer

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const puppeteer = require("puppeteer");
+const { chromium } = require('playwright');
+// const puppeteer = require("puppeteer");
 
 const main = async () => {
-  const browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-    headless: false,
-  });
-
-  const page = await browser.newPage();
+  // const browser = await puppeteer.launch({
+  //   executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
+  //   headless: false,
+  // });
+  const browser = await chromium.launch({ args: ['--no-sandbox'] })
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
   await page.goto('https://www.saltswap.finance/pools');
 
