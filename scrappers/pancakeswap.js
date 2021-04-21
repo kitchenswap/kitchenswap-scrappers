da05a5995dbeaa7f28e0bf4fd2fe23b1c978155d
hannawaffle <hanna.waffle@gmail.com>
2021-04-21 13:52:17 -0300

    fix pancakeswap

--- scrappers/pancakeswap.js
+++ scrappers/pancakeswap.js
@@ -3,7 +3,7 @@ const id = 'pancakeswap';
 const url = 'https://pancakeswap.finance/pools';
 
 var onPreScrap = () => {
-  const poolCards = document.querySelectorAll('div#root > div > div > div:nth-child(2) > div > div:nth-child(4) > div');
+  const poolCards = document.querySelectorAll('div#root > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div');
 
   [...poolCards].forEach((poolCard) => {
     const details = poolCard.querySelector(':scope > div:nth-child(2)');
@@ -23,7 +23,7 @@ var onPreScrap = () => {
 }
 
 var onScrap = () => { 
-  const poolCards = document.querySelectorAll('div#root > div > div > div:nth-child(2) > div > div:nth-child(4) > div');
+  const poolCards = document.querySelectorAll('div#root > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div');
 
   var info = [...poolCards].map((poolCard) => {
     const data = poolCard.querySelector(':scope > div:nth-child(1)');
@@ -36,7 +36,12 @@ var onScrap = () => {
 
     const earnToken = title.split(' ')[0];
     const stakeToken = 'CAKE';
-    const APR = data.querySelector(':scope > div:nth-child(6) > div:nth-child(2)').textContent;
+
+    const APRContainer = data.querySelector(':scope > div:nth-child(6) > div:nth-child(2)');
+    if (!APRContainer) {
+      return;
+    }
+    const APR = APRContainer.textContent;
 
     const details = poolCard.querySelector(':scope > div:nth-child(2)');
     const totalLiquidity = details.querySelector(':scope > div:nth-child(2) > div > div:nth-child(2)').textContent;
